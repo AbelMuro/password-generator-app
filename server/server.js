93aef8c1133c7ded9743309e20c2b8c4ef68b749
abelmuro <abelmuro93@gmail.com>
2023-12-16 14:05:01 -0800

    commit

--- server/server.js
+++ server/server.js
@@ -1,14 +1,24 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+const express = require ('express');
+const path = require ('path');
+const app = express ();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use (express.json ());
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// Your static pre-build assets folder
+app.use (express.static (path.join (__dirname, '..', 'build')));
+
+// Root Redirects to the pre-build assets
+app.get ('/', function (req,res) {
+  res.sendFile (path.join (__dirname, '..', 'src/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Any Page Redirects to the pre-build assets folder index.html that
+// will load the react app
+app.get ('*', function (req,res) {
+  res.sendFile (path.join (__dirname, '..', 'src/index.html'));
 });
+
+app.listen (port, ()=> {
+  console.log ("Server is running on port: ", port)
+})
