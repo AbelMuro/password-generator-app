4e03209caf38a8a55c25db540b8c7de4c9802da3
abelmuro <abelmuro93@gmail.com>
2023-12-16 13:48:04 -0800

    commit

--- scripts/heroku-start.js
+++ scripts/heroku-start.js
@@ -3,21 +3,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
 
-// Your static pre-build assets folder
-app.use(express.static(path.join(__dirname, '..', '')));
-
-// Root Redirects to the pre-build assets
-app.get('/', function(req,res){
-  res.sendFile(path.join(__dirname, '..', ''));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// Any Page Redirects to the pre-build assets folder index.html that // will load the react app
-app.get('*', function(req,res){
-  res.sendFile(path.join(__dirname, '..', '/index.html'));
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-
-app.listen(port, ()=>{
-  console.log("Server is running on port: ", port)
-})
